9ae6e89e4bdb8da87ef6b26c3c892874863790bb
Andre Steenveld <andre.steenveld@gmail.com>
2021-02-13 13:55:37 +0100

    Slicing the UI in bits

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,87 @@
+import { Component } from "react";
 import "./styles.css";
 
-export default function App() {
-  return (
-    <div className="App">
-      <input type="number" placeholder="A" />
-      <input type="number" placeholder="B" />
-      <hr />
-      <p>Data grid goes here</p>
-      <hr />
-      <p>Scatter plot goes here</p>
-    </div>
-  );
+export class Grid extends Component {
+    render() {
+        return (
+            <table>
+                <thead>
+                    <tr>
+                        <td colSpan={2}>
+                            Calibration formula: <i>y = a * x + b</i>
+                        </td>
+                        <td>
+                            Input and chart scale:
+                            <i>
+                                1e
+                                <input type="number" />
+                            </i>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td colSpan={3}>
+                            A: <input type="number" />
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td colSpan={3}>
+                            B: <input type="number" />
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <th scope="col">X Concentration</th>
+                        <th scope="col">Y (Theorectical)</th>
+                        <th scope="col">Y (Measured)</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    <tr>
+                        <td colSpan={3}>
+                            <i>empty</i>
+                        </td>
+                    </tr>
+                </tbody>
+
+                <tfoot>
+                    <tr>
+                        <th scope="col">
+                            <input type="number" />
+                        </th>
+                        <th scope="col"> &nbsp; </th>
+                        <th scope="col">
+                            <input type="number" />
+                        </th>
+                    </tr>
+                </tfoot>
+            </table>
+        );
+    }
 }
+
+export class Chart extends Component {
+    render() {
+        return (
+            <div>
+                <span>Chart goes here</span>
+            </div>
+        );
+    }
+}
+
+export class App extends Component {
+    render() {
+        return (
+            <div>
+                <Grid />
+                <hr />
+                <Chart />
+            </div>
+        );
+    }
+}
+
+export default App;
